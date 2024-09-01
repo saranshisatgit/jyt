8a8dd0b71673c380397b2c82736e728e78036aa1
Saransh Sharma <saranshis@protonmail.com>
2024-09-01 13:52:39 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -15,11 +15,11 @@ const nextConfig = withStoreConfig({
       },
       {
         protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+        hostname: "medusa-server-jyt.s3.us-east-1.amazonaws.com",
       },
       {
         protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
+        hostname: "medusa-server-jyt.s3.amazonaws.com",
       },
       {
         protocol: "https",
