7d46053fe30f53da2ef8c800e7b93613a84630f7
Saransh Sharma <saranshis@protonmail.com>
2024-09-01 14:52:09 +0200

    Branding Changes

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -8,9 +8,9 @@ import { ProductCollectionWithPreviews } from "types/global"
 import { cache } from "react"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Cici Label good stuff hand made.",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Cici Label brand focused on hand made clothes using AI and Tech for good.",
 }
 
 const getCollectionsWithProducts = cache(
