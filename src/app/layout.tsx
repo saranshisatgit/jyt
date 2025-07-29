c369bb88e4ea639e057e57f4c661d95e0d28ac86
Saransh Sharma <Saransh Sharma>
2025-07-29 11:25:39 +0530

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+      <head>
+        <script defer data-domain="shop.cicilabel.com" src="https://analytics.jaalyantra.com/js/script.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: 'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }'
+        }} />
+      </head>
       <body>
         <main className="relative">{props.children}</main>
       </body>
